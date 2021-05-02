73d6616efa4c7972cf67fa62fafa5369254c9198
Rawan.alakhras <ralakhras36@gmail.com>
2021-05-02 12:52:58 +0300

    add animals img

--- js/animal.js
+++ js/animal.js
@@ -1,6 +1,8 @@
 let row1=document.getElementById('row1');
 let row2=document.getElementById('row2');
 let row3=document.getElementById('row3');
+let row4=document.getElementById('row4');
+let row5=document.getElementById('row5');
 
 let animals=['bear.jpg','cat.png','cow.jpg','dog.png','elephant.png','fox.png','frog.png','giraffe.png','lion.jpg','monkey.png','panda.jpg','polar-bear.png','tiger.jpg'];
 let nameSound=['bear.mp3','cat.mp3','cow.mp3','dog.mp3','elephant.mp3','fox.mp3','frog.mp3','giraffe.mp3','lion.mp3','monkey.mp3','panda.mp3','polar-bear.mp3','tiger.mp3'];
@@ -22,27 +24,75 @@ function Animals(name,sound){
 Animals.all=[];
 
 for (let index = 0; index < animals.length; index++) {
-  new Animals(animals[index],nameSound[index]);
+  let newAnimal=new Animals(animals[index],nameSound[index]);
+  
 }
-Animals.prototype.rendur1=function(){
-
-  randomindex();
+Animals.prototype.rendur1 = function(index){
   let colElement=document.createElement('div');
-  row1.appendChild(colElement);
   let picElement=document.createElement('img');
-  picElement.src=Animals.all[forth].imagePath;
-  colElement.appendChild(picElement);
-  let button1=document.createElement('button');
-  button1.textContent=Animals.all[first].name;
-  colElement.appendChild(button1);
-  let button2=document.createElement('button');
-  button2.textContent=Animals.all[second].name;
-  colElement.appendChild(button2);
-  let button3=document.createElement('button');
-  button3.textContent=Animals.all[third].name;
-  colElement.appendChild(button3);
+  let NameH=document.createElement('h4');
+  let btn1=document.createElement('button');
+  let btn2=document.createElement('button');
+  btn1.textContent='sound';
+  btn2.textContent='Name';
+  colElement.appendChild(btn1);
+  colElement.appendChild(btn2);
+
+  
+  NameH.textContent=this.name;
+  colElement.appendChild(NameH);
+  if(index < 3){
+    row1.appendChild(colElement);
+    picElement.src=this.imagePath;
+    colElement.appendChild(picElement);
+
+
+  }else if(index >=3 && index<6){
+    row2.appendChild(colElement);
+    picElement.src=this.imagePath;
+    colElement.appendChild(picElement);
+
+  }else if(index >=6 && index <9){
+    row3.appendChild(colElement);
+    picElement.src=this.imagePath;
+    colElement.appendChild(picElement);
+
+  }
+  else if(index>=9 && index<12){
+
+    row4.appendChild(colElement);
+    picElement.src=this.imagePath;
+    colElement.appendChild(picElement);
+
+  }
+  else{
+    row5.appendChild(colElement);
+    picElement.src=this.imagePath;
+    colElement.appendChild(picElement);
+  }
+  let aduioSound=document.createElement('audio');
+  colElement.appendChild(aduioSound);
+  aduioSound.src=this.nameSoundPath;
+
+  let aduioSound2=document.createElement('audio');
+  colElement.appendChild(aduioSound2);
+  aduioSound2.src=this.animalSoundPath;
+  btn2.addEventListener('click',function(){
+   
+    aduioSound.play();
+
+  });
+  btn1.addEventListener('click',function(){
+   
+    aduioSound2.play();
+
+  });
+ 
 
 };
+for (let i=0;i<Animals.all.length;i++){
+  Animals.all[i].rendur1(i);
+}
 Animals.prototype.rendur2=function(){
 
   randomindex();
@@ -83,14 +133,14 @@ Animals.prototype.rendur3=function(){
 };
 console.log(Animals.all);
 
-for (let index = 0; index < 3; index++) {
+/* for (let index = 0; index < 3; index++) {
 
 
   Animals.all[i].rendur1();
   Animals.all[i].rendur2();
   Animals.all[i].rendur3();
 }
-
+ */
 
 function randomindex(){
 
